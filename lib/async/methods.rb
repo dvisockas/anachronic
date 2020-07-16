58126578d6a5120ee0bb2101c6f35f111e541de2
Danielius Visockas <danieliusvisockas@gmail.com>
2020-07-16 16:33:46 +0300

    Remove unnecessary method removal

--- lib/async/methods.rb
+++ lib/async/methods.rb
@@ -7,7 +7,6 @@ module Async
      def async(method_name)
       new_name = "async__#{method_name}".to_sym
       alias_method new_name, method_name
-      remove_method method_name
 
       define_method(method_name) do |*args|
         background_executor.perform_later(self, new_name, *args)
